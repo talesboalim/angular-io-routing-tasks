b53af91f5476f039031d481146802be6e058f837
Tales Boalim <talesboalim@gmail.com>
2021-11-07 10:59:52 -0300

    Rota filha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
-  {path:'first-component',component: FirstComponent},
-  {path:'second-component',component: SecondComponent}
+  { path:'first-component', component: FirstComponent},
+  { path:'second-component', component: SecondComponent,
+  children: [
+    {
+      path: 'child-a', // child route path
+      component: ChildAComponent, // child route component that the router renders
+    },
+    {
+      path: 'child-b',
+      component: ChildBComponent, // another child route component that the router renders
+    },
+  ],
+  },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }, 
 ];
 
 @NgModule({
